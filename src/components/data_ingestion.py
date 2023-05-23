678debbcfb12637080a31bf863cc2ec039aae3a2
Rakesh-Sri <rakeshkandula99@gmail.com>
2023-05-22 23:39:55 -0400

    Added: The code for data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass
 #below modules are created by the author
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -39,4 +41,10 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
-            
+
+if __name__ == "__main__":
+    obj=DataIngestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
